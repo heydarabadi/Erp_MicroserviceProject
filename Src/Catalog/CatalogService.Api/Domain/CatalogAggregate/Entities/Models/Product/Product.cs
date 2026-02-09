dac9def9dba6b78fe05b0de270243f311d7a1a62
arthas <siavashebrahimi3234@gmail.com>
2026-02-09 18:24:36 +0330

    product id and primary keys .

--- Src/Catalog/CatalogService.Api/Domain/CatalogAggregate/Entities/Models/Product/Product.cs
+++ Src/Catalog/CatalogService.Api/Domain/CatalogAggregate/Entities/Models/Product/Product.cs
@@ -1,11 +1,14 @@
 ﻿using CatalogService.Api.Domain.CatalogAggregate.Entities.Models.ProductToCategory;
 using CatalogService.Api.Domain.CatalogAggregate.ValueObjects.Objects.Product;
+using MongoDB.Bson;
 using Shared.Domain;
 
 namespace CatalogService.Api.Domain.CatalogAggregate.Entities.Models.Product;
 
-public class Product:AuditableAggregateRoot<Guid>
+public class Product:AuditableAggregateRoot<ObjectId>
 {
+    public ObjectId Id { get; private set; }
+    public Guid DomainId { get; private set; }
     public ProductName Name { get; private set; }
     public List<ProductToCategories> Categories { get; set; }
 }
